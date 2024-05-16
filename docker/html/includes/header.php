5582214e273ada5759db04986ae56d37d1bb8231
Yiming-Cao <1192702@student.roc-nijmegen.nl>
2024-05-16 12:03:57 +0200

    home pagina

---
+++ docker/html/includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div class="headerRight">
+
+        </div>
+        <div class="headerMid">
+            <div class="logo">
+                <img src="/images/logo.png" alt="" class="logo">
+            </div>
+        </div>
+        <div class="headerLeft">
+                <a href="../pages/register_logic.php">
+                    <div class="register" type="button" >
+                        <h1>register</h1>
+                    </div>
+                </a>
+                <a href="../pages/login.php">
+                    <div class="login" type="button" >
+                        <h2>login</h2>
+                    </div>
+                </a>
+</div>
+    </div>
+</body>
+</html>
--- docker/html/index.php
+++ docker/html/index.php
@@ -4,8 +4,84 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <?php
+        include 'includes/header.php';
+            
+    ?>
+
+    <div class="mid">
+        <div class="midup">
+            <div class="midup-left">
+                <img src="images/plane.png" alt="" class="planeimg">
+            </div>
+            <div class="midup-right">
+                <div class="midup-right-1">
+                    <h3>Welkom, Bonjour, </h3>
+                    <h3>Hola, aloha!</h3>
+                </div>
+                <div class="midup-right-2">
+                    <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum</h4>
+                </div>
+                <div class="midup-right-3">
+                    <h1>learn more about YAHI</h1>
+                </div>
+            </div>
+        </div>
+        <div class="middown">
+            <div class="middown-left">
+                <div class="middown-left-1">
+                    <h3>Plan your trip </h3>
+                    <h3>using our smart </h3>
+                    <h3>map!</h3>
+                </div>
+                <div class="middown-left-2">
+
+                </div>
+            </div>
+            <div class="middown-right">
+                <img src="images/wereldbol.png" alt="" class="wereldbolimg">
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-up">
+            <div class="footer-up-left">
+                <div class="footer-up-left-1">
+                    <h6>contact us?</h6>
+                </div>
+                <div class="footer-up-left-2">
+                    <div class="footer-up-left-2-vlak">
+                        <img src="images/tele.png" alt="">
+                        <h1>+00 000 000 000</h1>
+                    </div>
+                </div>
+                <div class="footer-up-left-2">
+                    <div class="footer-up-left-2-vlak">
+                        <img src="images/mail.png" alt="">
+                        <h1>ditIsEenEchteMail
+                            @yeetmail.com</h1>
+                    </div>
+                </div>
+                <div class="footer-up-left-2">
+                    <div class="footer-up-left-2-vlak">
+                        <img src="images/address.png" alt="">
+                        <h1>straatstraat nm.12</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-up-right">
+                <img src="images/logo-footer.png" alt="" class="logoimg">
+            </div>
+        </div>
+        <div class="footer-down">
+            <h5>YAHI is niet verantwoord voor eventuele ondoen van gezonodhheid, of mogenlijk verlies van leven onder ons lucht contract.</h5>
+        </div>
+    </footer>
 </body>
 </html>
