18424373cbcd0383d53bfe2d9578d01a1078051a
Yiming-Cao <1192702@student.roc-nijmegen.nl>
2024-05-22 10:59:36 +0200

    footer

---
+++ docker/html/includes/footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <footer>
+        <div class="footer-up">
+            <div class="footer-up-left">
+                <div class="footer-up-left-1">
+                    <h6>contact us?</h6>
+                </div>
+                <div class="footer-up-left-2">
+                    <div class="footer-up-left-2-vlak">
+                        <img src="images/tele.png" alt="">
+                        <h1>+00 000 000 000</h1>
+                    </div>
+                </div>
+                <div class="footer-up-left-2">
+                    <div class="footer-up-left-2-vlak">
+                        <img src="images/mail.png" alt="">
+                        <h1>ditIsEenEchteMail
+                            @yeetmail.com</h1>
+                    </div>
+                </div>
+                <div class="footer-up-left-2">
+                    <div class="footer-up-left-2-vlak">
+                        <img src="images/address.png" alt="">
+                        <h1>straatstraat nm.12</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-up-right">
+                <img src="images/logo-footer.png" alt="" class="logoimg">
+            </div>
+        </div>
+        <div class="footer-down">
+            <h5>YAHI is niet verantwoord voor eventuele ondoen van gezonodhheid, of mogenlijk verlies van leven onder ons lucht contract.</h5>
+        </div>
+    </footer>
+</body>
+</html>
