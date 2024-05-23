408a13a87eb5834a5fc3016c181040f3e59292bf
A-Benhaddi <abdelilahbenhaddi024@gmail.com>
2024-05-23 14:08:11 +0200

    Search

---
+++ docker/html/pages/search.php
@@ -0,0 +1,27 @@
+<?php
+    require 'conn.php'
+    $search = $_POST['search'];
+    $sql = "SELECT * FROM users WHERE username LIKE '%$search%' OR email LIKE '%$search%'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Username</th>
+            <th>Email</th>
+        </tr>
+    </table>
+    
+</body>
+</html>
