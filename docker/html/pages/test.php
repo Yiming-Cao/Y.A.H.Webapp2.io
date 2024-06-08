34784af80730d67215df76d69cc26ee05828311d
A-Benhaddi <abdelilahbenhaddi024@gmail.com>
2024-06-08 10:17:36 +0200

    opmaak

--- docker/html/pages/test.php
+++ docker/html/pages/test.php
@@ -1,6 +1,6 @@
 <?php
-  include 'conn.php';
-  ?>
+include 'conn.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,35 +8,34 @@
   <meta charset="UTF-8">
   <title>Document</title>
   <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Fuggles&family=Lato:wght@700&family=Montserrat:wght@500;700&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="/css/maincss.css">
 </head>
 
 <body>
-  
+
 
   </div>
   <header>
-  <div class="singleReis">';
-  '<img class="menu-fotos" src="../images/Tahiti.jpg" alt="">';
-    <section class='productenRow'>
-      <?php
-      // Variable statement is het resultaat van connectie & de query
-      $stmt = $connection->query("SELECT * FROM reizen");
+    <div class="singleReis">';
+      <section class='productenRow'>
+        <?php
+        // Variable statement is het resultaat van connectie & de query
+        $stmt = $connection->query("SELECT * FROM reizen");
 
-      // Loop voor alles wat we vinden
-      while ($row = $stmt->fetch()) {
-        echo '<div class="singleReis">';
-        echo '<img class="menu-fotos" src="../images/Tahiti.jpg" alt="">';
-        echo $row['naam'];
-        echo $row['prijs'];
-        echo $row['beschrijving'];
-        echo $row['imgslanden'];
+        // Loop voor alles wat we vinden
+        while ($row = $stmt->fetch()) {
+          echo '<div class="singleReis">';
+          echo '<img class="menu-fotos" src="../images/Tahiti.jpg" alt="">';
+          echo $row['naam'];
+          echo $row['prijs'];
+          echo $row['beschrijving'];
+          echo $row['imgslanden'];
 
-        echo '</div>';
-      }
-      ?>
-    </section>
+          echo '</div>';
+        }
+        ?>
+      </section>
 </body>
 </header>
 
