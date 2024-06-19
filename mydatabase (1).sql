3670db4192ed5f8d00122856b93db445503e8173
A-Benhaddi <abdelilahbenhaddi024@gmail.com>
2024-06-19 10:46:47 +0200

    De hele database

--- mydatabase (1).sql
+++ mydatabase (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Jun 14, 2024 at 11:51 AM
+-- Generation Time: Jun 19, 2024 at 08:46 AM
 -- Server version: 5.7.44
 -- PHP Version: 8.2.8
 
@@ -31,19 +31,20 @@ CREATE TABLE `bestemmingen` (
   `id` int(11) NOT NULL,
   `naam` varchar(100) NOT NULL,
   `land` varchar(50) NOT NULL,
-  `beschrijving` text NOT NULL
+  `beschrijving` text NOT NULL,
+  `HuurAuto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bestemmingen`
 --
 
-INSERT INTO `bestemmingen` (`id`, `naam`, `land`, `beschrijving`) VALUES
-(1, '1 week reizen', 'Tahiti', 'Kom een week reizen bij Tahiti'),
-(2, 'Hongkong', 'China', 'Hotel'),
-(3, 'saitama', 'Japan', 'Mooi appartement'),
-(4, 'Florida', 'Amerika', 'Een erg goedkoope appartement'),
-(5, 'Parijs', 'Frankrijk', 'De stad van love');
+INSERT INTO `bestemmingen` (`id`, `naam`, `land`, `beschrijving`, `HuurAuto`) VALUES
+(1, '1 week reizen', 'Tahiti', 'Kom een week reizen bij Tahiti', ''),
+(2, 'Hongkong', 'China', 'Hotel', ''),
+(3, 'saitama', 'Japan', 'Mooi appartement', ''),
+(4, 'Florida', 'Amerika', 'Een erg goedkoope appartement', ''),
+(5, 'Parijs', 'Frankrijk', 'De stad van love', '');
 
 -- --------------------------------------------------------
 
@@ -52,24 +53,25 @@ INSERT INTO `bestemmingen` (`id`, `naam`, `land`, `beschrijving`) VALUES
 --
 
 CREATE TABLE `boekingen` (
-  `boeking_id` int(255) NOT NULL,
+  `id` int(255) NOT NULL,
   `user_id` int(255) NOT NULL,
   `reis_id` int(255) NOT NULL,
-  `boekingDatum` date NOT NULL,
   `persoonen` int(90) NOT NULL,
-  `deTotalePrijs` decimal(65,0) NOT NULL
+  `deTotalePrijs` decimal(65,0) NOT NULL,
+  `startdatum` date DEFAULT NULL,
+  `einddatum` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `boekingen`
 --
 
-INSERT INTO `boekingen` (`boeking_id`, `user_id`, `reis_id`, `boekingDatum`, `persoonen`, `deTotalePrijs`) VALUES
-(1, 1, 1, '2024-06-12', 1, 240),
-(2, 2, 2, '2024-06-07', 1, 260),
-(3, 3, 3, '2024-06-12', 1, 300),
-(4, 4, 4, '2024-06-12', 1, 360),
-(5, 5, 5, '2024-06-12', 1, 370);
+INSERT INTO `boekingen` (`id`, `user_id`, `reis_id`, `persoonen`, `deTotalePrijs`, `startdatum`, `einddatum`) VALUES
+(1, 1, 1, 1, 240, NULL, NULL),
+(2, 2, 2, 1, 260, NULL, NULL),
+(3, 3, 3, 1, 300, NULL, NULL),
+(4, 4, 4, 1, 360, NULL, NULL),
+(5, 5, 5, 1, 370, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -89,13 +91,11 @@ CREATE TABLE `image` (
 --
 
 CREATE TABLE `reizen` (
-  `reizen_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `naam` varchar(100) NOT NULL,
   `beschrijving` text NOT NULL,
   `prijs` decimal(10,2) NOT NULL,
   `bestemmingen_id` int(11) NOT NULL,
-  `startDatum` date NOT NULL,
-  `eindDatum` date NOT NULL,
   `file` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -103,12 +103,12 @@ CREATE TABLE `reizen` (
 -- Dumping data for table `reizen`
 --
 
-INSERT INTO `reizen` (`reizen_id`, `naam`, `beschrijving`, `prijs`, `bestemmingen_id`, `startDatum`, `eindDatum`, `file`) VALUES
-(1, 'Italië', 'Een mooie tropische eiland', 260.00, 1, '2024-07-08', '2024-07-29', 'italië.jpg'),
-(2, 'China', 'Een mooie vlieg rit ', 300.00, 2, '2024-06-19', '2024-07-19', 'hongkong.jpg'),
-(3, 'Japan', 'Een erg populaire plek voor touristen', 150.00, 3, '2024-06-19', '2024-06-30', 'saitama.jpg'),
-(4, 'Amerika', 'Met een prachtige vlieg rit', 360.00, 4, '2024-06-19', '2024-07-25', 'florida.jpeg'),
-(5, 'Frankrijk', 'Een prachtige vlieg rit naar Parijs', 200.00, 5, '2024-06-19', '2024-07-25', 'parijs.jpg');
+INSERT INTO `reizen` (`id`, `naam`, `beschrijving`, `prijs`, `bestemmingen_id`, `file`) VALUES
+(1, 'Italië', 'Een mooie tropische eiland', 260.00, 1, 'italië.jpg'),
+(2, 'China', 'Een mooie vlieg rit ', 300.00, 2, 'hongkong.jpg'),
+(3, 'Japan', 'Een erg populaire plek voor touristen', 150.00, 3, 'saitama.jpg'),
+(4, 'Amerika', 'Met een prachtige vlieg rit', 360.00, 4, 'florida.jpeg'),
+(5, 'Frankrijk', 'Een prachtige vlieg rit naar Parijs', 200.00, 5, 'parijs.jpg');
 
 -- --------------------------------------------------------
 
@@ -142,18 +142,19 @@ CREATE TABLE `user` (
   `id` int(255) NOT NULL,
   `username` text NOT NULL,
   `email` text NOT NULL,
-  `password` text NOT NULL
+  `password` text NOT NULL,
+  `user_data_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
-(4, 'aaaaa', 'aaaaa.@aaa.com', 'aa'),
-(5, 'yiming', '1216894517@qq.com', '123'),
-(6, 'h', 'h@h.com', 'h'),
-(7, 'bruh', 'abdelilahbenhaddi024@gmail.com', 'bruh');
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `user_data_id`) VALUES
+(4, 'aaaaa', 'aaaaa.@aaa.com', 'aa', 0),
+(5, 'yiming', '1216894517@qq.com', '123', 0),
+(6, 'h', 'h@h.com', 'h', 0),
+(7, 'bruh', 'abdelilahbenhaddi024@gmail.com', 'bruh', 0);
 
 -- --------------------------------------------------------
 
@@ -162,7 +163,7 @@ INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
 --
 
 CREATE TABLE `user_data` (
-  `user_Data_id` int(255) NOT NULL,
+  `id` int(255) NOT NULL,
   `voornaam` varchar(90) NOT NULL,
   `achternaam` varchar(90) NOT NULL,
   `leeftijd` int(90) NOT NULL,
@@ -173,7 +174,7 @@ CREATE TABLE `user_data` (
 -- Dumping data for table `user_data`
 --
 
-INSERT INTO `user_data` (`user_Data_id`, `voornaam`, `achternaam`, `leeftijd`, `woonadres`) VALUES
+INSERT INTO `user_data` (`id`, `voornaam`, `achternaam`, `leeftijd`, `woonadres`) VALUES
 (1, 'Abdelilah', 'Benhaddi', 20, 'Watertorstraat,22 6533PV');
 
 --
@@ -190,7 +191,7 @@ ALTER TABLE `bestemmingen`
 -- Indexes for table `boekingen`
 --
 ALTER TABLE `boekingen`
-  ADD PRIMARY KEY (`boeking_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `image`
@@ -202,7 +203,7 @@ ALTER TABLE `image`
 -- Indexes for table `reizen`
 --
 ALTER TABLE `reizen`
-  ADD PRIMARY KEY (`reizen_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `search`
@@ -220,7 +221,7 @@ ALTER TABLE `user`
 -- Indexes for table `user_data`
 --
 ALTER TABLE `user_data`
-  ADD PRIMARY KEY (`user_Data_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -236,7 +237,7 @@ ALTER TABLE `bestemmingen`
 -- AUTO_INCREMENT for table `boekingen`
 --
 ALTER TABLE `boekingen`
-  MODIFY `boeking_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `image`
@@ -248,7 +249,7 @@ ALTER TABLE `image`
 -- AUTO_INCREMENT for table `reizen`
 --
 ALTER TABLE `reizen`
-  MODIFY `reizen_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `search`
@@ -266,7 +267,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user_data`
 --
 ALTER TABLE `user_data`
-  MODIFY `user_Data_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
