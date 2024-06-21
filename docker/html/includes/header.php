8da4a5a43fa4dae74cd1bbbce9d7d8905f38c380
Yiming-Cao <1192702@student.roc-nijmegen.nl>
2024-06-21 14:27:55 +0200

    fix profile

--- docker/html/includes/header.php
+++ docker/html/includes/header.php
@@ -24,7 +24,7 @@
         <div class="headerLeft">
             <?php if (isset($_SESSION['user'])): ?>
                 <!-- 如果用户已登录，显示用户名 -->
-                <a href="../pages/user-detail.php">
+                <a href="../../pages/user-detail.php">
                     <div class="register" type="button" ><h1><?php echo htmlspecialchars($_SESSION['user']); ?></h1></div>
                 </a>
                 <a href="logout_logic.php">
